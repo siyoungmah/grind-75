94263b686daafe626bc7c945accfb1ea9b350658
Si Young Mah <siyoungmah@gmail.com>
2023-05-19 12:28:57 -0700

    Add spiral-matrix

---
+++ challenges/spiral-matrix.js
@@ -0,0 +1,37 @@
+// Link: https://leetcode.com/problems/spiral-matrix/
+// Difficulty: Medium
+// Time: 25 min
+
+/**
+ * Given an m x n matrix, return all elements of the matrix in spiral order.
+ * 
+ * Example 1:
+ * [1,2,3]
+ * [4,5,6]
+ * [7,8,9]
+ * 
+ * Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+ * Output: [1,2,3,6,9,8,7,4,5]
+ * 
+ * Example 2:
+ * [1, 2, 3, 4]
+ * [5, 6, 7, 8]
+ * [9,10,11,12]
+ * 
+ * Input: matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12]]
+ * Output: [1,2,3,4,8,12,11,10,9,5,6,7]
+ * 
+ * Constraints:
+ * m == matrix.length 
+ * n == matrix[i].length
+ * 1 <= m, n <= 10
+ * -100 <= matrix[i][j] <= 100
+ */
+
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+const spiralMatrix = (matrix) => {
+    
+};
